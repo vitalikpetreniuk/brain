5b635bd6f0989d64dea0737303aadfce74c2bcc9
vitalikpetreniuk <petreniuk.ua@gmail.com>
2018-07-13 21:22:12 +0300

    fixed sidebar fixed

--- js/brain.js
+++ js/brain.js
@@ -59,6 +59,14 @@ $(function(){
         });
     }
 
+    if($('.br-sfixed').length)
+    {
+        $(".br-sfixed").stick_in_parent({
+            parent: '.br-row-main',
+            offset_top: 120
+        });
+    }
+
     if($('.br-pt-lc').length){
         $('.br-pt-lc select').barrating({
             theme: 'fontawesome-stars'
@@ -792,50 +800,6 @@ $(function(){
     {
 
         var clipboard = new Clipboard('.br-pcode');
-    }
-
-    if($('.br-sfixed').length)
-    {
-        var a = document.querySelector('.br-sfixed'), b = null, P = 0;  // если ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента. Может быть отрицательным числом
-        window.addEventListener('scroll', Ascroll, false);
-        document.body.addEventListener('scroll', Ascroll, false);
-        function Ascroll() {
-            if (b == null) {
-                var Sa = getComputedStyle(a, ''), s = '';
-                for (var i = 0; i < Sa.length; i++) {
-                    if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-                        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
-                    }
-                }
-                b = document.createElement('div');
-                b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-                a.insertBefore(b, a.firstChild);
-                var l = a.childNodes.length;
-                for (var i = 1; i < l; i++) {
-                    b.appendChild(a.childNodes[1]);
-                }
-                a.style.height = b.getBoundingClientRect().height + 'px';
-                a.style.padding = '0';
-                a.style.border = '0';
-            }
-            var Ra = a.getBoundingClientRect(),
-                R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.br-pad').getBoundingClientRect().top + 0);  // селектор блока, при достижении верхнего края которого нужно открепить прилипающий элемент;  Math.round() только для IE; если ноль заменить на число, то блок будет прилипать до того, как нижний край элемента дойдёт до футера
-            if ((Ra.top - 120) <= 0) {
-                if ((Ra.top - 120) <= R) {
-                    b.className = 'stop';
-                    b.style.top = - R +'px';
-                } else {
-                    b.className = 'sticky';
-                    b.style.top = 120 + 'px';
-                }
-            } else {
-                b.className = '';
-                b.style.top = '';
-            }
-            window.addEventListener('resize', function() {
-                a.children[0].style.width = getComputedStyle(a, '').width
-            }, false);
-        }
     }
 
     if($('.br-accordion').length)
